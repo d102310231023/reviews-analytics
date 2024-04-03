cc8035ff86eedc3f93612835c46cb3af01de29b3
Derek Chen <d102310231023@gmail.com>
2024-04-03 16:53:05 +0800

    add new[]

--- read.py
+++ read.py
@@ -3,9 +3,13 @@ count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		count += 1
-		if count % 1000 == 0:
-			print(len(data))
-print(len(data))
 
-print(data[0])
+sumlength = 0
+for d in data:
+	sumlength =sumlength + len(d)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print(len(new))		
